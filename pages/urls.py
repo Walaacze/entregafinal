ee61300dc23fd61546aec19c58e0080ca11bcd66
Pablo Garcia <pablo.a.garcia96@gmail.com>
2022-04-18 19:50:16 -0300

    correciones

--- pages/urls.py
+++ pages/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogs/', views.ListaBlogs.as_view(), name='lista_blogs'),
-    # path('detalle/', views.DetalleBlogs.as_view(), name='detalle_blog'),
-    # path('crear/', views.CrearBlog.as_view(), name='crear_blog'),
-    # path('borrar/', views.BorrarBlog.as_view(), name='borrar_blog'),
-    # path('actualizar/', views.ActualizarBlog.as_view(), name='actualizar_blog'),
-
-
+    path('', views.ListaBlogs.as_view(), name='lista_blogs'),
+    path('detalle/<int:pk>', views.DetalleBlogs.as_view(), name='detalle_blogs'),
+    path('crear/', views.CrearBlog.as_view(), name='crear_blog'),
+    path('borrar/', views.BorrarBlog.as_view(), name='borrar_blog'),
+    path('actualizar/', views.ActualizarBlog.as_view(), name='actualizar_blog'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,23 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from pages.models import Blog
 
 class ListaBlogs(ListView):
     model = Blog
     template_name = 'pages/lista_blogs.html'
-
+    
+class DetalleBlogs(DetailView):
+    model = Blog
+    template_name = 'pages/detalle_blogs.html'
+    
+class CrearBlog(ListView):
+    model = Blog
+    success_url = '/pages/'
+    fields = ['titulo', 'subtitulo', 'cuerpo', 'autor', 'fecha', 'imagen']
+    
+class BorrarBlog(ListView):
+    model = Blog
+    success_url = '/pages/'
+    fields = ['titulo', 'subtitulo', 'cuerpo', 'autor', 'fecha', 'imagen']
+class ActualizarBlog(ListView):
+    model = Blog
+    success_url = '/pages/'
